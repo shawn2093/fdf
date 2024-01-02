c4ef3b6399306e780d3a443696eb3be826667100
shawn2093 <shawnong1993@gmail.com>
2024-01-02 23:08:49 +0800

    try out

--- test.c
+++ test.c
@@ -3,22 +3,53 @@
 #include "libft/includes/libft.h"
 #include "libft/includes/get_next_line.h"
 #include <time.h>
+#include <math.h>
 
 #define WIDTH 1000
 #define HEIGHT 700
 
+void drawline(void *win, void *mlx, int x1, int x2, int y1, int y2, int **matrix)
+{
+	// double a1 = ((x1 - y1) * cos(0.8)) * 50 + 100;
+	// double b1 = (((x1 + y1)) * sin(0.8) - matrix[x1][y1]) * 50 + 100;
+	// double a2 = (x2 - y2) * cos(0.8) * 50 + 100;
+	// double b2 = (((x2 + y2)) * sin(0.8) - matrix[x2][y2]) * 50 + 100;
+	// double a1 = ((x1 - y1) * cos(0.8)) * 50 + 100;
+	// double b1 = ((x1 + y1)) * sin(0.8) - matrix[x1][y1];
+	// double a2 = (x2 - y2) * cos(0.8);
+	// double b2 = ((x2 + y2)) * sin(0.8) - matrix[x2][y2];
+	int a1 = x1 * 50 + 100;
+	int b1 = y1 * 50 + 100;
+	int a2 = x2 * 50 + 100;
+	int b2 = y2 * 50 + 100;
+	(void) matrix;
+	for (int y = b1; y < b2; ++y)
+	{
+		if (y2 == y1)
+		{
+			for (int x = a1; x < a2; ++x)
+				mlx_pixel_put(mlx, win, x, y, 0xffffff);
+		}
+		else
+		{
+			float	x;
+			x = (y - b1) * (a2 - a1) / (b2 - b1) + a1;
+			mlx_pixel_put(mlx, win, x, y, 0xffffff);
+		}
+	}
+}
 
 int	main(int ac, char **av)
 {
-	// void	*win;
-	// void	*mlx;
+	void	*win;
+	void	*mlx;
 
 	// srand(time(NULL));
-	// mlx = mlx_init();
-	// win = mlx_new_window(mlx, 
-	// 			WIDTH,
-	// 			HEIGHT,
-	// 			"Pollock");
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 
+				WIDTH,
+				HEIGHT,
+				"Pollock");
 	// for (int y = HEIGHT * 0.1; y < HEIGHT * 0.9; ++y)
 	// {
 	// 	for (int x = WIDTH * 0.1; x < WIDTH * 0.9; ++x)
@@ -69,26 +100,67 @@ int	main(int ac, char **av)
 	char *str = get_next_line(fd);
 	t_gnl_list	*input = NULL;
 	t_gnl_list *node;
+	char	**splitstr;
+	int		width;
+	int		i;
+	width = 0;
 	while (str)
 	{
+		i = -1;
 		node = (t_gnl_list *)malloc(sizeof(t_gnl_list));
 		node->str = ft_strdup(str);
 		node->next = NULL;
-		ft_printf("%s", node->str);
+		splitstr = ft_split(node->str, ' ');
+		while (splitstr[++i])
+			free(splitstr[i]);
+		free(splitstr);
+		i--;
+		if (!width)
+			width = i;
+		else
+		{
+			if (width != i)
+			{
+				perror("Width not tallied");
+				exit(EXIT_FAILURE);
+			}
+		}
 		ft_lstadd_back_gnl(&input, node);
 		free(str);
 		str = get_next_line(fd);
 	}
-	// char	**splitstr = word_count(input->str, ' ');
 	int height = ft_lstsize_gnl(input);
-	int	width = word_count(input->str, ' ');
-	ft_printf("height: %d width: %d", height, width);
-	// node = *input;
-	// ft_printf("done\n");
-	// while (node)
-	// {
-	// 	ft_printf("%s\n", node->content);
-	// 	node = node->next;
-	// }
+	int	**matrix;
+	matrix = (int **) malloc(sizeof(int *) * height);
+	i = -1;
+	int j;
+	node = input;
+	while (++i < height)
+	{
+		j = -1;
+		matrix[i] = (int *) malloc(sizeof(int) * width);
+		splitstr = ft_split(node->str, ' ');
+		while (++j < width)
+		{
+			matrix[i][j] = ft_atoi(splitstr[j]);
+			free(splitstr[j]);
+		}
+		free(splitstr);
+		node = node->next;
+	}
+	i = -1;
+	while (++i < height)
+	{
+		j = -1;
+		while (++j < width)
+		{
+			if (i + 1 != height)
+				drawline(win, mlx, i, i + 1, j, j, matrix);
+			if (j + 1 != width)
+				drawline(win, mlx, i, i, j, j + 1, matrix);
+		}
+	}
+	// void drawline(void *win, void *mlx, int x1, int x2, int y1, int y2, int **matrix)
+	mlx_loop(mlx);
 	return (0);
 }
